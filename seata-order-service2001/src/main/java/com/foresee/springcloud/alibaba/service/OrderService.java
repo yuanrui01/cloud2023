f032c69c00713bd772abc39071edab7f833891f6
yuanrui01 <xdyrfree@gmail.com>
2023-08-08 03:25:28 +0800

    添加Seata示例应用

---
+++ seata-order-service2001/src/main/java/com/foresee/springcloud/alibaba/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.foresee.springcloud.alibaba.service;
+
+import com.foresee.springcloud.alibaba.domain.Order;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-26 15:19
+ */
+public interface OrderService
+{
+    void create(Order order);
+}
---
+++ seata-storage-service2002/src/main/java/com/foresee/springcloud/alibaba/controller/StorageController.java
@@ -0,0 +1,24 @@
+package com.foresee.springcloud.alibaba.controller;
+
+
+import com.foresee.springcloud.alibaba.domain.CommonResult ;
+import com.foresee.springcloud.alibaba.service.StorageService ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StorageController {
+
+    @Autowired
+    private StorageService storageService;
+
+    /**
+     * 扣减库存
+     */
+    @RequestMapping("/storage/decrease")
+    public CommonResult decrease(Long productId, Integer count) {
+        storageService.decrease(productId, count);
+        return new CommonResult(200,"扣减库存成功！");
+    }
+}
